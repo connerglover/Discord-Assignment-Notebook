009a2f65f6028d1fc430e279abbbcbd9eaf0ff3f
Conner <conner.glover@startmail.com>
2023-01-29 13:50:34 -0600

    saving stuff i forgot to save

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function sendToLink() {
-    const webhookUrl = document.getElementById("webhook").value;
-    location.href('https://assignment-notebook.netlify.app/notebook?webhook_url=' + webhookUrl)
+  const webhookUrl = document.getElementById("webhook").value;
+  const finalLink = "https://assignment-notebook.netlify.app/notebook?webhook_url=" + webhookUrl
+  console.log(finalLink);
+  window.location.replace(finalLink);
 }
